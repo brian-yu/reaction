8e4b8857b0186629401500a9a49738a9dd1ebabf
Brian Yu <brianyu8@gmail.com>
2015-10-22 11:57:44 -0400

    Finish timer

--- app/src/main/java/io/byu/login/SignedInActivity.java
+++ app/src/main/java/io/byu/login/SignedInActivity.java
@@ -10,9 +10,16 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
+import java.util.Collection;
 import java.util.Date;
+import java.util.*;
+import java.util.TreeMap;
 
 public class SignedInActivity extends AppCompatActivity {
 
@@ -20,7 +27,7 @@ public class SignedInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Firebase.setAndroidContext(this);
-        Firebase ref = new Firebase("https://loginandroid.firebaseio.com/");
+        Firebase ref = new Firebase("https://loginandroid.firebaseio.com/scores");
         setContentView(R.layout.activity_signed_in);
 
         Intent intent = getIntent();
@@ -32,9 +39,31 @@ public class SignedInActivity extends AppCompatActivity {
 
         greet.setText("Hello " + email + "! \nWhen the box turns green, press it as fast as you can!");
 
+        final class Score {
+            private long time;
+            private String email;
+            public Score() {}
+            public Score(String email, long time) {
+                this.email = email;
+                this.time = time;
+            }
+            public long getTime() {
+                return time;
+            }
+            public String getEmail() {
+                return email;
+            }
+        }
 
+//        public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+//            Score newScore = dataSnapshot.getValue(Score.class);
+////                System.out.println("Email: " + newScore.getEmail());
+////                System.out.println("Time: " + newScore.getTime());
+//            scores.setText("Most Recent Score:\n" + newScore.getEmail() + ": " + String.valueOf(newScore.getTime()));
+//        }
 
         box.setOnClickListener(new View.OnClickListener() {
+            Firebase ref = new Firebase("https://loginandroid.firebaseio.com/scores");
             int count =0;
             long start = System.currentTimeMillis();
             Handler handler = new Handler();
@@ -59,13 +88,15 @@ public class SignedInActivity extends AppCompatActivity {
                     handler.postDelayed(timer, 3000);
 
 
-                } else if(count == 2 && canPress == true) {
+                } else if (count == 2 && canPress == true) {
                     long stop = System.currentTimeMillis();
                     box.setBackgroundColor(Color.parseColor("#8e44ad"));
 
                     long time = stop - start;
 
                     box.setText(String.valueOf(time) + "ms\nTap again to restart.");
+                    Score score = new Score(email, time);
+                    ref.setValue(score);
 
                 } else if(count == 2 && canPress == false) {
                     box.setBackgroundColor(Color.parseColor("#e67e22"));
