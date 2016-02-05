a7f568cfc1af357c80bf125c7b71c4cafcddef00
Jihyeong Lee <leejh1998@hotmail.com>
2016-02-05 11:52:11 -0500

    finished leaderboard

--- app/src/main/java/io/byu/reaction/LeaderActivity.java
+++ app/src/main/java/io/byu/reaction/LeaderActivity.java
@@ -26,10 +26,10 @@ public class LeaderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leader);
-        final TextView scores = (TextView)findViewById(R.id.scores);
-        Firebase ref = new Firebase("https://loginandroid.firebaseio.com/scores");
-        Query queryRef = ref.orderByValue();
-        queryRef.addChildEventListener(new ChildEventListener() {
+        final TextView rScores = (TextView)findViewById(R.id.rScores);
+        Firebase ReactRef = new Firebase("https://loginandroid.firebaseio.com/scores/reaction");
+        Query rQueryRef = ReactRef.orderByValue();
+        rQueryRef.addChildEventListener(new ChildEventListener() {
             TreeMap<String, Long> scoreMap = new TreeMap<>();
 
             @Override
@@ -52,7 +52,56 @@ public class LeaderActivity extends AppCompatActivity {
                     }
 
 
-                scores.setText(str);
+                rScores.setText(str);
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+        Firebase sRef = new Firebase("https://loginandroid.firebaseio.com/scores/speed");
+        Query sQueryRef = sRef.orderByValue();
+        final TextView sScores = (TextView)findViewById(R.id.sScores);
+        sQueryRef.addChildEventListener(new ChildEventListener() {
+            TreeMap<String, Long> scoreMap = new TreeMap<>();
+
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                TreeMap<String, Long> newScore = dataSnapshot.getValue(TreeMap.class);
+
+//                System.out.println("Email: " + newScore.getEmail());
+//                System.out.println("Time: " + newScore.getTime());
+                Set temp = newScore.keySet();
+                Object[] lol = temp.toArray();
+                Object lol2 = lol[0];
+                String lol3 = lol2.toString().replace("@DOT@", ".");
+                scoreMap.put(lol3, newScore.get(lol2));
+
+                String str = "";
+                for(Map.Entry<String,Long> entry : scoreMap.entrySet()) {
+                    String k = entry.getKey();
+                    String v = String.valueOf(entry.getValue());
+                    str += k + ": " + v + " Taps\n";
+                }
+
+
+                sScores.setText(str);
             }
 
             @Override
